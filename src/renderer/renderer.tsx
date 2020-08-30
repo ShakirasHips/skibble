ef1b6a7106db1f233573907735e8eb9dca9ed79c
Jayden <jaydenobrien96@gmail.com>
2020-08-30 15:24:53 +1000

    added button

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -6,11 +6,63 @@ import * as ReactDOM from 'react-dom';
 
 // Import the styles here to process them with webpack
 import '@public/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Fade } from 'react-bootstrap'
+import Confetti from 'react-dom-confetti'
 
+
+const ConfettiConfig = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 20,
+  elementCount: 200,
+  dragFriction: 0.1,
+  duration: 3000,
+  stagger: 2,
+  width: "10px",
+  height: "10px",
+  perspective: "500px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
+
+
+class ClickMe extends React.Component<{}>{
+  state = {fade: false};
+  
+  componentDidUpdate() {
+    if (this.state.fade) {
+      setTimeout(() => {
+        this.reset()
+      },3000)
+    }
+  }
+
+  reset(): void {
+    this.setState(() => ({
+      fade: false
+    }))
+  }
+
+  ClickHandler(): void {
+    this.setState(state => ({
+      fade: true
+    }))
+  }
+
+  render(): any {
+    const {fade} = this.state;
+
+    return (
+      <div className='centered'>
+        <Button variant="primary" onClick={() => this.ClickHandler()}>
+          {this.state.fade ? 'Katherine' : 'Who does Jayden love?'}
+        </Button> 
+        <Confetti active={ fade } config={ ConfettiConfig }></Confetti>   
+      </div>
+      )
+  }
+}
 ReactDOM.render(
-  <div className='app'>
-    <h1>I love you Katherine</h1>
-    
-  </div>,
+  <ClickMe />,
   document.getElementById('app')
 );
